b081799562acf3559d117fc5d5fc0516afc0124d
savior42 <savior42@72bbbca6-d526-0410-a7d9-f06f51895060>
2013-08-15 16:45:23 +0000

    rule_condition_tree.* added, and is already used in fomacg2.l, although only for testing purposes.
    A    rule_condition_tree.c
    A    rule_condition_tree.h
    M    fomacg2.l
    
    
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/branches/fomacg@46533 72bbbca6-d526-0410-a7d9-f06f51895060

---
+++ rule_condition_tree.c
@@ -0,0 +1,206 @@
+#include "rule_condition_tree.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+// TODO: to parameter
+#define MAX_STATES 1000
+
+/**
+ * Comparison function for qsort() that sorts the rules first by section, then
+ * by size.
+ */
+static int rule_compare(const void* rule1, const void* rule2) {
+  struct cg_rules* r1 = *((struct cg_rules **)rule1);
+  struct cg_rules* r2 = *((struct cg_rules **)rule2);
+  if (r1->section_no < r2->section_no) {
+    return -1;
+  } else if (r1->section_no > r2->section_no) {
+    return 1;
+  } else {
+    if (r1->conditions->statecount < r2->conditions->statecount) {
+      return -1;
+    } else if (r1->conditions->statecount > r2->conditions->statecount) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+}
+
+/** Comparison function for qsort() that sorts the tree(-node)s by size. */
+static int tree_compare(const void* tree1, const void* tree2) {
+  struct Node* t1 = *((struct Node**) tree1);
+  struct Node* t2 = *((struct Node**) tree2);
+  /* NULL goes to the end. */
+  if (t1 == NULL) {
+    return t2 != NULL ? 1 : 0;
+  } else {
+    if (t2 == NULL) {
+      return -1;
+    } else {
+      if (t1->fsa->statecount < t2->fsa->statecount) {
+        return -1;
+      } else if (t1->fsa->statecount > t2->fsa->statecount) {
+        return 1;
+      } else {
+        return 0;
+      }
+    }
+  }
+}
+
+/**
+ * Builds the binary tree for the current section.
+ * @param rules the rules array.
+ * @paran begin the index of the first rule in the section.
+ * @param length the length of the section.
+ * @return the tree.
+ */
+static struct Node* build_section_tree(struct cg_rules* rules[],
+                                       size_t begin, size_t length) {
+  fprintf(stderr, "build_section_tree(%zu, %zu)\n", begin, length);
+  struct Node** trees = calloc(length, sizeof(struct Node*));
+  /* Let's fill the array. */
+  int i;
+  for (i = 0; i < length; i++) {
+    int rule = begin + i;
+    trees[i] = calloc(1, sizeof(struct Node));
+    trees[i]->fsa = rules[rule]->conditions;
+    trees[i]->fst = rules[rule]->rule;
+    trees[i]->section = rules[rule]->section_no;
+    trees[i]->no_rules = 1;
+    fprintf(stderr, "section %s : %d\n", rules[rule]->conditions->name, rules[rule]->section_no);
+  }
+
+  int trees_still = length;
+  /** And now: smallest-first union. */
+  while (trees_still > 1 && trees[0]->fsa->statecount < MAX_STATES
+                         && trees[1]->fsa->statecount < MAX_STATES) {
+    struct Node* new_tree = calloc(1, sizeof(struct Node));
+    new_tree->fsa = fsm_topsort(
+                      fsm_minimize(
+                        fsm_determinize(
+                          fsm_union(
+                            fsm_copy(trees[0]->fsa),
+                            fsm_copy(trees[1]->fsa)))));
+    fsm_sort_arcs(new_tree->fsa, 1);
+    sprintf(new_tree->fsa->name, "union");
+    new_tree->left  = trees[0];
+    new_tree->right = trees[1];
+    new_tree->section = trees[0]->section;
+    new_tree->no_rules = trees[0]->no_rules + trees[1]->no_rules;
+    trees[0] = NULL;
+    trees[1] = new_tree;
+    qsort(trees, length, sizeof(struct Node*), tree_compare);
+    trees_still--;
+  }
+
+  /* Finishing touches... */
+  struct Node* ret = trees[0];
+  for (i = 0; i < length && trees[i] != NULL; i++) {
+    trees[i]->next = trees[i + 1];
+    if (strncmp(trees[i]->fsa->name, "C_", 2)) {
+      snprintf(trees[i]->fsa->name, 40, "S_%d_%d", trees[i]->section, i);
+    }
+  }
+
+  struct Node* p = trees[0];
+  while (p != NULL) {
+    fprintf(stderr, "Tree %s : %d (%zu)\n", p->fsa->name, p->fsa->statecount, p->no_rules);
+    p = p->next;
+  }
+
+  free(trees);
+  return ret;
+}
+
+/**
+ * Called by create_binary_tree() to add the newly created tree group @p trees
+ * to the returned set (which starts at @p ret and ends in @p last). Updates the
+ * latter two.
+ */
+static void add_trees_to_ret(struct Node* trees,
+                             struct Node** ret, struct Node** last) {
+  if (*ret == NULL) {
+    *ret = *last = trees;
+    for (; (*last)->next != NULL; *last = (*last)->next);
+  } else {
+    (*last)->next = trees;
+    for (; (*last)->next != NULL; *last = (*last)->next);
+  }
+}
+
+struct Node* create_binary_tree(struct cg_rules* cg_rules, size_t no_rules) {
+  struct cg_rules** rules = calloc(no_rules, sizeof(struct cg_rules*));
+  struct Node* ret = NULL;
+  struct Node* last = NULL;
+  int i;
+
+  for (i = 0; i < no_rules; i++) {
+    rules[i] = cg_rules;
+    cg_rules = cg_rules->next;
+  }
+
+  /* First, we sort the rules by section and size. */
+  qsort(rules, no_rules, sizeof(struct cg_rules*), rule_compare);
+  /* XXX */
+  for (i = 0; i < no_rules; i++) {
+    fprintf(stderr, "Rule %s : %d\n", rules[i]->conditions->name, rules[i]->conditions->statecount);
+  }
+  fprintf(stderr, "Total number of rules: %zu\n", no_rules);
+
+  /* Then, we build the tree groups section-by-section. */
+  int curr_section = rules[0]->section_no;
+  int section_first = 0;
+
+  for (i = 0; i < no_rules; i++) {
+    if (rules[i]->section_no != curr_section) {
+      struct Node* trees = build_section_tree(rules, section_first,
+                                              i - section_first);
+      add_trees_to_ret(trees, &ret, &last);
+      curr_section = rules[i]->section_no;
+      section_first = i;
+    }
+  }
+  struct Node* trees = build_section_tree(rules, section_first,
+                                          i - section_first);
+  add_trees_to_ret(trees, &ret, &last);
+
+  free(rules);
+  return ret;
+}
+
+/**
+ * Counts the non-NULL struct fsm*'s in @p tree. When you call this func tion,
+ * @p count should be @c 0.
+ */
+static size_t count_fsms(struct Node* tree, size_t count) {
+  if (tree->fsa != NULL) count++;
+  if (tree->fst != NULL) count++;
+  if (tree->left != NULL) count = count_fsms(tree->left, count);
+  if (tree->right != NULL) count = count_fsms(tree->right, count);
+  if (tree->next != NULL) count = count_fsms(tree->next, count);
+  return count;
+}
+
+/** Fills @p rules with the rules and conditions in @p tree. */
+static size_t fill_fsms(struct Node* tree, struct fsm** rules, size_t i) {
+  if (tree->fsa != NULL) rules[i++] = tree->fsa;
+  if (tree->fst != NULL) rules[i++] = tree->fst;
+  if (tree->left != NULL) i = fill_fsms(tree->left, rules, i);
+  if (tree->right != NULL) i = fill_fsms(tree->right, rules, i);
+  if (tree->next != NULL) i = fill_fsms(tree->next, rules, i);
+  return i;
+}
+
+struct fsm** serialize_tree(struct Node* tree, size_t* num_rules) {
+  *num_rules = count_fsms(tree, 0);
+  struct fsm** rules = calloc(*num_rules, sizeof(struct fsm*));
+  fill_fsms(tree, rules, 0);
+  return rules;
+}
+
+struct Node* deserialize_tree(struct fsm* rules[], size_t num_rules) {
+  return NULL;
+}
