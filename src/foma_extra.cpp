1a06f96072dc7b48276c825079d00b8cffd5575a
savior42 <savior42@72bbbca6-d526-0410-a7d9-f06f51895060>
2013-09-03 10:24:25 +0000

    Binary search extracted from apply_detmin_fsa() to find_transition().
    (Also added an initial, not functioning version of apply_detmin_fst(), commented
    out -- disregard it for now.)
    
    M    src/foma_extra.cpp
    M    src/foma_extra.h
    
    
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/branches/fomacg@47150 72bbbca6-d526-0410-a7d9-f06f51895060

--- src/foma_extra.cpp
+++ src/foma_extra.cpp
@@ -1,5 +1,7 @@
 #include "foma_extra.h"
 
+#include <sstream>
+
 bool apply_detmin_fsa(struct apply_handle *h, const char *word) {
   static int count = 0;
   h->instring = const_cast<char*>(word);
@@ -15,7 +17,7 @@ bool apply_detmin_fsa(struct apply_handle *h, const char *word) {
 
   int i;
   for (i = 0; i < h->sigmatch_array_size;) {
-Detmin_Outer:
+Detmin_Fsa_Outer:
     /* Is this word[i] == 0. */
     if ((h->sigmatch_array+i)->signumber == 0 || word[i] == 0) break;
     //	fprintf(stderr, "Reading %d (%c) ...\n",
@@ -36,25 +38,13 @@ Detmin_Outer:
       curr_state = tr->target;
       i += (h->sigmatch_array+i)->consumes;
       //	    fprintf(stderr, " found!\n");
-      goto Detmin_Outer;
+      goto Detmin_Fsa_Outer;
     } else {
-      int begin = *(h->statemap + curr_state);
-      int end   = begin + *(h->numlines + curr_state);
-      while (begin < end) {
-        count++;
-        int middle = (begin + end) / 2;
-        struct fsm_state* tr = transitions + middle;
-        //	    fprintf(stderr, "begin: %d, end: %d, middle: %d, symbol at middle: %d, symbol: %d(%c)\n",
-        //		    begin, end, middle, tr->in, (h->sigmatch_array+i)->signumber, word[i]);
-        if (tr->in == (h->sigmatch_array+i)->signumber) {
-          curr_state = tr->target;
-          i += (h->sigmatch_array+i)->consumes;
-          goto Detmin_Outer;
-        } else if ((h->sigmatch_array+i)->signumber < tr->in) {
-          end = middle;
-        } else {
-          begin = middle + 1;
-        }
+      struct fsm_state* tr = find_transition(h, curr_state, i);
+      if (tr != NULL) {
+        curr_state = tr->target;
+        i += (h->sigmatch_array + i)->consumes;
+        goto Detmin_Fsa_Outer;
       }
       //	    for (j = 0; j < *(h->numlines + curr_state); j++) {
       //		count++;
@@ -83,5 +73,108 @@ Detmin_Outer:
     return false;
   }
   //    fprintf(stderr, "That's all folks!\n");
+}
+
+//std::string apply_detmin_fst(struct apply_handle *h, const char *word) {
+//  std::istringstream ret;
+//  static int count = 0;
+//  h->instring = const_cast<char*>(word);
+//  /* Also sets h->current_instring_length. */
+//  apply_create_sigmatch(h);
+//
+//  struct fsm* fst = h->last_net;
+//  //    fprintf(stderr, "Sorted: %d, arity: %d\n", fst->arcs_sorted_in, fst->arity);
+//  if (fst == NULL || fst->finalcount == 0) {
+//    return NULL;
+//  }
+//  struct fsm_state* transitions = fst->states;
+//  int curr_state = 0;
+//
+//  // TODO: use h->outstring
+//  h->iptr = NULL; h->ptr = 0; h->ipos = 0; h->opos = 0;
+//
+//  while (true) {
+//    /* If we hit an accepting state after consuming the input: return! */
+//    if (h->ipos == h->current_instring_length &&
+//        (h->gstates+h->ptr)->finals_state == 1) {
+//      // TODO to char* 
+//      return ret.str();
+//    }
+//Detmin_Fst_Outer:
+//    /* Is this word[i] == 0. */
+//    if ((h->sigmatch_array+i)->signumber == 0 || word[i] == 0) break;
+//    //	fprintf(stderr, "Reading %d (%c) ...\n",
+//    //		(h->sigmatch_array+i)->signumber, word[i]);
+//    /* Linear search for the moment. */
+//    //	fprintf(stderr, "numlines for state %d: %d\n", curr_state, *(h->numlines+curr_state));
+//    if (*(h->numlines+curr_state) == 0) return false;  /* I think... */
+//    int j = 0;
+//    //	fprintf(stderr, "Checking transitions %d(%d) through %d(%d)...\n",
+//    //		*(h->statemap + curr_state), j,
+//    //		*(h->statemap + curr_state) + *(h->numlines + curr_state),
+//    //		*(h->numlines + curr_state));
+//    /* Assumption: FSAs don't support UNKNOWN; detmin FSAs are epsilon-free. */
+//    if ((h->sigmatch_array+i)->signumber == IDENTITY) {
+//      //	    fprintf(stderr, "%d <= IDENTITY\n", (h->sigmatch_array+i)->signumber);
+//      struct fsm_state* tr = transitions + *(h->statemap + curr_state);
+//      //	    fprintf(stderr, "%dth transition: in %d, target: %d\n", j, tr->in, tr->target);
+//      curr_state = tr->target;
+//      i += (h->sigmatch_array+i)->consumes;
+//      //	    fprintf(stderr, " found!\n");
+//      goto Detmin_Fst_Outer;
+//    } else {
+//      struct fsm_state* tr = find_transition(h, curr_state, i);
+//      if (tr != NULL) {
+//        curr_state = tr->target;
+//        ipos += (h->sigmatch_array + ipos)->consumes;
+//        goto Detmin_Fst_Outer;
+//      }
+//      //	    for (j = 0; j < *(h->numlines + curr_state); j++) {
+//      //		count++;
+//      //		struct fsm_state* tr = transitions + *(h->statemap + curr_state) + j;
+//      //    //	    fprintf(stderr, "%dth transition: in %d, target: %d\n", j, tr->in, tr->target);
+//      //		if (tr->in == (h->sigmatch_array+i)->signumber) {
+//      //		    curr_state = tr->target;
+//      //		    i += (h->sigmatch_array+i)->consumes;
+//      //    //		fprintf(stderr, " found!\n");
+//      //		    goto Detmin_Outer;
+//      //		} else {
+//      //    //		fprintf(stderr, " not found!\n");
+//      //		}
+//      //	    }
+//    }
+//    //	fprintf(stderr, "Count: %d\n", count);
+//    return false;  /* I think... */
+//  }
+//  /* TODO: final state? */
+//  if ((transitions + *(h->statemap + curr_state))->final_state) {
+//    //	fprintf(stderr, "Count: %d\n", count);
+//    return true;
+//  } else {
+//    //	fprintf(stderr, "Not in final state!\n");
+//    //	fprintf(stderr, "Count: %d\n", count);
+//    return false;
+//  }
+//  //    fprintf(stderr, "That's all folks!\n");
+//}
+
+// TODO curr_state, ipos to h
+struct fsm_state* find_transition(struct apply_handle *h, int curr_state, int& ipos) {
+  int begin = *(h->statemap + curr_state);
+  int end   = begin + *(h->numlines + curr_state);
+  while (begin < end) {
+    int middle = (begin + end) / 2;
+    struct fsm_state* tr = h->gstates + middle;
+    //	    fprintf(stderr, "begin: %d, end: %d, middle: %d, symbol at middle: %d, symbol: %d(%c)\n",
+    //		    begin, end, middle, tr->in, (h->sigmatch_array+i)->signumber, word[i]);
+    if (tr->in == (h->sigmatch_array + ipos)->signumber) {
+      return tr;
+    } else if ((h->sigmatch_array + ipos)->signumber < tr->in) {
+      end = middle;
+    } else {
+      begin = middle + 1;
+    }
+  }
+  return NULL;
 }
 
--- src/foma_extra.h
+++ src/foma_extra.h
@@ -11,4 +11,17 @@
  */
 bool apply_detmin_fsa(struct apply_handle *h, const char *word);
 
+/**
+ * This function runs a deterministic, minimal, FST much faster than the regular
+ * apply_down() function. By "deterministic" we mean that if from a state, there
+ * is a transition where the input is the epsilon symbol, but there are other
+ * transitions as well, the latter will get higher priority. The arcs must be
+ * sorted.
+ */
+//std::string apply_detmin_fst(struct apply_handle *h, const char *word);
+
+/** Finds the transition for the current state and input via binary search. Returns @c NULL if it could not be found. */
+inline struct fsm_state* find_transition(struct apply_handle *h, int curr_state, int& ipos);
+// TODO: up & down
+
 #endif
