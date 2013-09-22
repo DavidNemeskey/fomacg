347ad558c7dd983cecca7337f4a1906029facea8
savior42 <savior42@72bbbca6-d526-0410-a7d9-f06f51895060>
2013-09-22 20:06:26 +0000

    Added a -I so that size_t can be used.
    M    Makefile.sh
    
    
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/branches/fomacg@47528 72bbbca6-d526-0410-a7d9-f06f51895060

--- src/experimental/Makefile.sh
+++ src/experimental/Makefile.sh
@@ -1,1 +1,2 @@
-g++ -o am main.cpp automata_mista.cpp
+# -I/usr/include is needed because http://stackoverflow.com/questions/1107940/size-t-can-not-be-found-by-g-4-1-or-others-on-ubuntu-8-1
+g++ -o am main.cpp automata_mista.cpp -I/usr/include
