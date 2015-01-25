bf7a8b746427f2fe378d9c891ac94572b99e7cd8
Dávid <david@cliqz.com>
2015-01-25 22:30:24 +0100

    Added a basic implementation of the trie I plan to use for rule filtering. Also trying to use C++11 stuff.

---
+++ src/tester_trie.h
@@ -0,0 +1,79 @@
+#pragma once
+
+#include <cstdlib>
+
+#include <memory>
+#include <iostream>
+
+
+/******************************** Header part ********************************/
+
+
+/** Simple trie class that supports a payloading mechanism. */
+template <class Code, class Payload>
+class Trie {
+public:
+  Trie(size_t branching, Payload* payload=nullptr);
+  ~Trie();
+
+  /**
+   * Adds a new branch to this trie. Does nothing if the branch already exists.
+   */
+  Trie* add(const Code& code, Payload* payload=nullptr);
+  
+  template <class InputIterator>
+  Trie* add_branch(InputIterator first, InputIterator last,
+                   Payload* payload=nullptr);
+
+private:
+  /** Sets the payload; used by the constructor & add methods. */
+  void set_payload(Payload* payload=nullptr);
+
+  size_t branching;
+  std::unique_ptr<std::unique_ptr<Trie>[]> branches;  /* Array of pointers. */
+  std::unique_ptr<Payload> payload;
+};
+
+
+/**************************** Implementation part ****************************/
+
+
+template <class Code, class Payload>
+Trie<Code, Payload>::Trie(size_t _branching, Payload* _payload) : branching{_branching} {
+  branches.reset(new std::unique_ptr<Trie>[_branching]);
+  set_payload(_payload);
+}
+template <class Code, class Payload>
+Trie<Code, Payload>::~Trie() {
+  std::cout << "Deleting Trie at " << this << "..." << std::endl;
+}
+
+template <class Code, class Payload>
+Trie<Code, Payload>* Trie<Code, Payload>::add(
+    const Code& _code, Payload* _payload) {
+  Trie* next = branches.get()[_code].get();
+  if (next == nullptr) {
+    next = new Trie(branching, _payload);
+    branches.get()[_code].reset(next);
+  }
+  return next;
+}
+
+template <class Code, class Payload>
+template <class InputIterator>
+Trie<Code, Payload>* Trie<Code, Payload>::add_branch(
+    InputIterator first, InputIterator last, Payload* _payload) {
+  Trie* curr = this;
+  while (first != last) {
+    curr = curr->add(*first);
+    ++first;
+  }
+  curr->set_payload(_payload);
+  return curr;
+}
+
+template <class Code, class Payload>
+void Trie<Code, Payload>::set_payload(Payload* _payload) {
+  payload.reset(_payload);
+}
+
---
+++ src/trie_test.cpp
@@ -0,0 +1,15 @@
+#include "tester_trie.h"
+
+#include <array>
+#include <set>
+
+typedef Trie<int, std::set<int> > Tree;
+
+int main(int argc, char* argv[]) {
+  Tree t(1024);
+  Tree* t2 = t.add(3, nullptr);
+  Tree* t3 = t2->add(15, nullptr);
+  std::array<size_t, 3> arr = {1, 2, 3};
+  std::set<int>* pl = new std::set<int>{4, 5, 6};
+  t.add_branch(begin(arr), end(arr), pl);
+}
